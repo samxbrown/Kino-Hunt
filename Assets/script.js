7038142c8aec78b9f98f24747f127f8eb2fa386b
kanggeon123 <kanggeon216@gmail.com>
2023-07-25 13:19:01 +0900

    Added console command to get list of movies if genre is chosen

--- Assets/script.js
+++ Assets/script.js
@@ -17,11 +17,14 @@ function getApi() {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
         for(var i = 0; i < data.genres.length; i++){
             var listItem = document.createElement('button');
-            console.log(data.genres[i].name);
             listItem.textContent = data.genres[i].name;
+            listItem.setAttribute('id',data.genres[i].id);
+            listItem.setAttribute('class','genre');
+            listItem.addEventListener('click', function() {
+                getMovieList(this.id);
+            })
             genreList.appendChild(listItem);
         }
       });
@@ -30,3 +33,25 @@ function getApi() {
   }
 
 startButton.addEventListener('click', getApi);
+
+function getMovieList(clicked_id) {
+    var requestUrl = 'https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=' + clicked_id + '&page=1';
+  
+    var options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cb55d331c2msheffcf3623997c3bp1fc0f2jsn2b8f997e3c9f',
+            'X-RapidAPI-Host': 'advanced-movie-search.p.rapidapi.com'
+        }
+    };
+
+    fetch(requestUrl,options)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        for(var i = 0; i < data.results.length; i++){
+            console.log(data.results[i].original_title);
+        }
+      });
+}
